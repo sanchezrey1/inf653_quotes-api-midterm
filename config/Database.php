8e6c16e0cbe53db17185fb4cfb1380d825edbd35
Reyes Sanchez <r_sanchez6@mail.fhsu.edu>
2026-03-25 21:25:07 -0500

    Fix database connection parsing for Render

--- config/Database.php
+++ config/Database.php
@@ -1,29 +1,21 @@
 <?php
 class Database {
-    private $host;
-    private $port;
-    private $db_name;
-    private $username;
-    private $password;
-    public $conn;
-
-    public function __construct() {
-        $this->host = getenv('DB_HOST');
-        $this->port = getenv('DB_PORT');
-        $this->db_name = getenv('DB_NAME');
-        $this->username = getenv('DB_USER');
-        $this->password = getenv('DB_PASSWORD');
-    }
+    private $conn;
 
     public function connect() {
         $this->conn = null;
 
         try {
-            $dsn = "pgsql:host={$this->host};port={$this->port};dbname={$this->db_name};sslmode=require";
-            $this->conn = new PDO($dsn, $this->username, $this->password);
+            $this->conn = new PDO(
+                "pgsql:host=" . $_ENV['DB_HOST'] . ";port=" . $_ENV['DB_PORT'] . ";dbname=" . $_ENV['DB_NAME'],
+                $_ENV['DB_USER'],
+                $_ENV['DB_PASS']
+            );
+
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
         } catch(PDOException $e) {
-            echo json_encode(array('message' => $e->getMessage()));
+            echo "Connection Error: " . $e->getMessage();
         }
 
         return $this->conn;
